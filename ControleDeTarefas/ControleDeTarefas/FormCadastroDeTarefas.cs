7c5d49de43924fc4e3c67cd25a66d9e23b203368
Bruno Henrique <90804273+BrunoHenrique80@users.noreply.github.com>
2022-06-17 19:48:08 -0300

    Projeto controle de tarefa

---
+++ ControleDeTarefas/ControleDeTarefas/FormCadastroDeTarefas.cs
@@ -0,0 +1,54 @@
+﻿using BLL;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControleDeTarefas
+{
+    public partial class FormCadastroDeTarefas : Form
+    {
+        public FormCadastroDeTarefas()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSalvar_Click(object sender, EventArgs e)
+        {
+            TarefaBLL tarefaBLL = new TarefaBLL();
+            Tarefa tarefa = new Tarefa();
+            tarefa.Id = Convert.ToInt32(idTextBox.Text);
+            tarefa.Descricao = descricaoTextBox.Text;
+            tarefa.Estatus = estatusTextBox.Text;
+            tarefa.Id_Usuario = 1;
+            tarefaBLL.Inserir(tarefa);
+            MessageBox.Show("Operação realizada com sucesso!");
+            tarefaBindingSource.DataSource = typeof(Tarefa);
+            tarefaBindingSource.AddNew();
+            idTextBox.Focus();
+        }
+
+     
+
+        private void FormCadastroDeTarefas_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void estatusTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSairDoLogin_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ ControleDeTarefas/ControleDeTarefas/TeladeLogin.cs
@@ -0,0 +1,54 @@
+﻿using BLL;
+using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace ControleDeTarefas
+{
+    public partial class TeladeLogin : Form
+    {
+        public bool Logou;
+        public TeladeLogin()
+        {
+            InitializeComponent();
+            Logou = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void buttonEntrada_Click(object sender, EventArgs e)
+        {
+            UsuarioBLL usuarioBLL = new UsuarioBLL();
+            BindingSource usuarioBindingSource = new BindingSource();
+            usuarioBindingSource.DataSource = usuarioBLL.Buscar(textBoxUsuario.Text);
+
+            if (usuarioBindingSource.Count != 0)
+            {
+                string nome = ((DataRowView)usuarioBindingSource.Current).Row["Nome"].ToString();
+                string senha = ((DataRowView)usuarioBindingSource.Current).Row["Senha"].ToString();
+
+                if (nome == textBoxUsuario.Text && senha == textBoxSenha.Text)
+                {
+                    Logou = true;
+                    Close();
+                }
+                else
+                {
+                    MessageBox.Show("Usuário ou senha incorreta!");
+                    textBoxSenha.Text = "";
+                    textBoxSenha.Focus();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Usuário ou senha incorreta!");
+                textBoxSenha.Text = "";
+                textBoxSenha.Focus();
+            }
+        }
+    }
+}        
+
